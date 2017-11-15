4ccf250b8c6cf102c1ede87804c4bff0aad0aa07
dinuka <dinukasal@gmail.com>
2017-11-15 16:15:15 +0000

    added travis and pug

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var port = process.env.PORT || 8080;
 var mongoUri = process.env.MONGOLAB_URI ||
-        process.env.MONGOHQ_URL ||
-        'mongodb://localhost/video';
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/video';
 
 var app = express();
-
 
 // configure our server
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,43 +24,53 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// pug configuration
+const pug = require('pug');
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 
 // make sure we can connect to database before starting server
-MongoClient.connect(mongoUri, function(err, db) {
+MongoClient.connect(mongoUri, function (err, db) {
 
     assert.equal(null, err);
     console.log('Successfully connected to mondodb');
 
-    app.get('/', function(req, res) {
-        db.collection('movies').find({}).toArray(function(err, docs) {
-            res.render('index', {'movies': docs} );
+    app.get('/', function (req, res) {
+        db.collection('movies').find({}).toArray(function (err, docs) {
+            //res.render('index', { 'movies': docs });
+            res.render("home", { 'movie': docs });
         });
     });
 
-    app.post('/', function(req, res) {
+    app.get('/add', function (req, res) {
+        db.collection('movies').find({}).toArray(function (err, docs) {
+            res.render('index', { 'movies': docs });
+        });
+    });
+
+    app.post('/', function (req, res) {
         var title = req.body.movieTitle;
         var year = req.body.movieYear;
         var imdb = req.body.movieIMDB;
 
         db.collection('movies').insertOne({
-                                            title: title,
-                                            year: year,
-                                            imdb: imdb
-                                        }, function(err, doc) {
-                                            assert.equal(null, err);
-                                            res.render('newmovie', {movie: req.body});
-                                        }
+            title: title,
+            year: year,
+            imdb: imdb
+        }, function (err, doc) {
+            assert.equal(null, err);
+            res.render('newmovie', { movie: req.body });
+        }
         );
 
     });
 
     // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-      var err = new Error('Not Found');
-      err.status = 404;
-      next(err);
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
     });
 
     // error handlers
@@ -69,28 +78,28 @@ MongoClient.connect(mongoUri, function(err, db) {
     // development error handler
     // will print stacktrace
     if (app.get('env') === 'development') {
-      app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-          message: err.message,
-          error: err
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
         });
-      });
     }
 
     // production error handler
     // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: {}
-      });
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
 
 
 
-    app.listen(port, function() {
+    app.listen(port, function () {
         console.log('Server listening on port 8080');
     });
 
